ae3094438aa91f0abbce5ab7e4924df912131772
Lembit Valgma <lembit.valgma@gmail.com>
2016-11-23 22:54:54 +0200

    bugfix

--- filemanager.py
+++ filemanager.py
@@ -26,15 +26,13 @@ class FileManager(Thread):
             for smith in self.wordsmiths.values():
                 self.write_smith(smith)
         # to ensure ensure it...
-        while not self.stopped.wait(60):
-            for smith in self.wordsmiths.values():
-                self.write_smith(smith)
+        for smith in self.wordsmiths.values():
+            self.write_smith(smith)
 
     def write_smith(self,smith):
         pth = os.path.join(self.folder,smith.filename)
         with open(pth, 'w') as f:
             f.write(smith.content())
-
 
     def get_all_titles(self):
         return os.listdir(self.folder)
